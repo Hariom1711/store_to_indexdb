68afb7673ae53eb33f3b5100f75b8e9d3a784190
Hariom Ojha <2003om1711@gmail.com>
2023-05-11 11:08:50 +0530

    updating

--- src/App.js
+++ src/App.js
@@ -1,59 +1,57 @@
-import React, { useState } from "react";
-import "./App.css";
-import Compressor from "compressorjs";
+import './App.css';
+import { db } from './db';
 
 function App() {
-  const [imageData, setImageData] = useState("");
-
-  const handleChange = (e) => {
-    const file = e.target.files[0];
-    new Compressor(file, {
-      quality: 0.6,
-      success: (compressedResult) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(compressedResult);
-        reader.onload = (event) => {
-          setImageData(event.target.result);
-        };
-      },
-      error: (error) => {
-        console.log("Error occurred while compressing image: ", error);
-      },
-    });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const file = e.target.elements.file.files[0];
+    const name = e.target.elements.name.value;
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = async (e) => {
+      const dataUrl = e.target.result;
+      const icon = { name, dataUrl };
+      await db.icons.put(icon);
+      alert('Image saved to IndexedDB!');
+    };
   };
 
-  const saveToIndexDB = () => {
-    const request = window.indexedDB.open("MyDatabase", 1);
-    request.onerror = (event) => {
-      console.log("Database error: ", event.target.errorCode);
-    };
-    request.onsuccess = (event) => {
-      const db = event.target.result;
-      const transaction = db.transaction(["MyObjectStore"], "readwrite");
-      const objectStore = transaction.objectStore("MyObjectStore");
-      const data = { image: imageData };
-      const request = objectStore.add(data);
-      request.onsuccess = (event) => {
-        console.log("Image added to IndexedDB");
-      };
-      request.onerror = (event) => {
-        console.log("Error occurred while adding image to IndexedDB: ", event.target.errorCode);
-      };
-    };
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const name = e.target.elements.name.value;
+    const icon = await db.icons.get(name);
+    if (icon) {
+
+  const img = document.getElementById('img');
+  img.src = icon.dataUrl;
+
+     
+      alert('Image found in IndexedDB!');
+    } else {
+      alert('Image not found in IndexedDB!');
+    }
   };
 
   return (
     <div className="App">
-      <h1>Image Compression and Storage</h1>
-      <input type="file" accept="image/*" onChange={handleChange} />
-      <button onClick={saveToIndexDB} disabled={!imageData}>
-        Save to IndexedDB
-      </button>
-      {imageData && (
-        <div>
-          <img src={imageData} alt="Selected" style={{ maxWidth: "300px" }} />
-        </div>
-      )}
+      <h1>IndexedDB Image Storage</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Icon Name:</label>
+        <input type="text" id="name" name="name" />
+        <br />
+        <label htmlFor="file">Icon Image:</label>
+        <input type="file" id="file" name="file" />
+        <br />
+        <button type="submit">Save Image</button>
+      </form>
+      <br />
+      <form onSubmit={handleSearch}>
+        <label htmlFor="name">Search Icon:</label>
+        <input type="text" id="name" name="name" />
+        <button type="submit">Search</button>
+      </form>
+      <br />
+      <img id="img" src="" alt="" width={300} height={300}/>
     </div>
   );
 }
